c3af6b15f0369add0f289af50f45673fc328ece4
Anant Govil <44673944+a-govil@users.noreply.github.com>
2022-11-02 19:31:53 +0530

    build errors fix

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/anantgovil/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/anantgovil/Developer/flutter-ui-wallet-cryptocurrency"
+export "FLUTTER_APPLICATION_PATH=/Users/anantgovil/Developer/crypto-wallet"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/anantgovil/Developer/flutter-ui-wallet-cryptocurrency/lib/main.dart"
+export "FLUTTER_TARGET=/Users/anantgovil/Developer/crypto-wallet/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/anantgovil/Developer/flutter-ui-wallet-cryptocurrency/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/anantgovil/Developer/crypto-wallet/.dart_tool/package_config.json"
